99dc4d7dae4a5ba9008aa4ba716670e318d9769a
marcio-mfm <104047028+marcio-mfm@users.noreply.github.com>
2024-03-15 17:01:38 -0400

    First commit

---
+++ src/app/app.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modulo2;
+
+/**
+ *
+ * @author Marcio
+ */
+public class Modulo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ContaCorrente c1 = new ContaCorrente();
+        
+        c1.deposito(100);
+        c1.saque(125);
+        c1.rendimento();
+        c1.exibeSaldo();
+        
+    }
+    
+}
